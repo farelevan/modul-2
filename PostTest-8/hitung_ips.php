d9625ffcbc097ac4b679d7b34f1500c1ad422fba
farelevan <farel.evan04@gmail.com>
2023-12-01 21:51:09 +0700

    PHP

--- PostTest-8/hitung_ips.php
+++ PostTest-8/hitung_ips.php
@@ -2,23 +2,42 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $jumlahMataKuliah = count($_POST) - 1; // Menghitung jumlah mata kuliah (dikurangi satu karena ada tombol submit)
 
-    $totalNilai = 0;
+    $totalBobot = 0;
+    $totalSKS = 0;
+
     foreach ($_POST as $key => $value) {
-        // Menghitung total nilai
+        // Menghitung total bobot dan total SKS
         if (strpos($key, 'matkul') === 0) {
-            $totalNilai += (float)$value;
+            $bobot = (float)$value;
+            $totalBobot += $bobot;
+
+            // Anggap setiap mata kuliah memiliki 1 SKS
+            $totalSKS += 1;
         }
     }
 
-    // Menghitung IPS
-    $ips = $totalNilai / $jumlahMataKuliah;
+    // Menghitung IPS rata-rata
+    if ($totalSKS > 0) {
+        $ips = $totalBobot / $totalSKS;
+    } else {
+        $ips = 0; // Menghindari pembagian dengan nol
+    }
 
     // Menentukan status lulus
-    $statusLulus = ($ips > 2) ? "Lulus" : "Tidak Lulus";
+    $statusLulus = ($ips >= 2) ? "Lulus" : "Tidak Lulus";
 
     // Menampilkan hasil
     echo "<h2>Hasil Perhitungan IPS</h2>";
     echo "<p>IPS: $ips</p>";
     echo "<p>Status Kelulusan: $statusLulus</p>";
+
+    // Tombol kembali
+    echo '<br><button onclick="kembali()">Kembali</button>';
 }
 ?>
+
+<script>
+    function kembali() {
+        window.history.back();
+    }
+</script>
